668c5b8c870306ff08d110410412502cc5bcd6dd
sikkzz <sling0623@gmail.com>
2024-12-10 21:21:17 +0900

    refactor: my page body skeleton add (#191)

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -4,6 +4,7 @@ import { ServerFetchBoundary } from "@/apis/ServerFetchBoundary";
 
 import Gnb from "@/components/common/Gnb/Gnb";
 import MyPageBody from "@/components/MyPage/MypageBody/MyPageBody";
+import MyPageBodySkeleton from "@/components/MyPage/MypageBody/MyPageBodySkeleton";
 import MyPageHeader from "@/components/MyPage/MyPageHeader/MyPageHeader";
 
 import { userInfoQueryOptions } from "@/hooks/api/userInfo/useUserInfoQuery";
@@ -14,20 +15,7 @@ export default function MyPage() {
     <>
       <MyPageHeader />
 
-      <Suspense
-        fallback={
-          // TODO: loading 컴포넌트로 변경
-          <div className="flex justify-between mt-[60px] px-4 animate-pulse">
-            <div className="flex gap-4 items-center">
-              <div className="w-[60px] h-[60px] rounded-full bg-gray-50" />
-              <div className="flex flex-col gap-1 items-start">
-                <div className="w-20 h-4 rounded bg-gray-50" />
-                <div className="w-14 h-5 rounded bg-gray-50" />
-              </div>
-            </div>
-          </div>
-        }
-      >
+      <Suspense fallback={<MyPageBodySkeleton />}>
         <ServerFetchBoundary fetchOptions={serverFetchOptions}>
           <MyPageBody />
         </ServerFetchBoundary>
